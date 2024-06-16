f4503a4357305f559723cae74bde75a9f85ceb69
Ruu <wmucheche@gmail.com>
2024-06-16 18:14:51 -0400

    Add code for minor todo list project

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("CONNECTED");
+let input = prompt("What would you like to do?");
+
+const todos = [];
+
+while (input.toLowerCase() !== "quit") {
+  if (input.toLowerCase() === "add") {
+    let task = prompt("Enter the task you have to do?");
+    todos.push(task);
+    alert("TODO ADDED");
+  } else if (input.toLowerCase() === "list") {
+    console.log("****************************");
+    for (let i = 0; i < todos.length; i++) {
+      console.log(`${i}: ${todos[i]}`);
+    }
+    console.log("****************************");
+  } else if (input.toLowerCase() === "delete") {
+    let deleteTodo = parseInt(prompt("Enter index of todo to delete"));
+    while (Number.isNaN(deleteTodo)) {
+      alert("ENTER A PROPER INDEX");
+      deleteTodo = parseInt(prompt("Enter index of todo to delete"));
+    }
+    todos.splice(deleteTodo, 1);
+    alert("DELETED TODO");
+  } else {
+    alert("CHOOSE A PROPER TASK!");
+  }
+  input = prompt("What would you like to do?");
+}
+
+console.log("YOU QUIT THE APP!");
